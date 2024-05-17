89b3bb657b905703006ca299f610e4a0fcadd5f7
김태경_laptop <98coding.gmail.com>
2024-05-17 12:01:52 +0900

    second day first commit

--- final_pjt/final_pjt_front/src/App.vue
+++ final_pjt/final_pjt_front/src/App.vue
@@ -4,15 +4,14 @@
     <div v-if="!store.isLogin">
       <RouterLink :to="{ name: 'home'}">Home</RouterLink> | 
       <RouterLink :to="{ name: 'login'}">로그인</RouterLink> | 
-      <RouterLink :to="{ name: 'signup'}">회원 가입</RouterLink> | 
-      <RouterLink :to="{ name: 'products'}">전체 상품 목록</RouterLink>
+      <RouterLink :to="{ name: 'signup'}">회원 가입</RouterLink>
     </div>
     <!-- 로그인이 된 경우 -> home, 로그아웃, 전체 상품 목록 조회 -->
     <!-- 회원 탈퇴의 경우에는 마이페이지에서 사용 -->
     <div v-else>
       <RouterLink :to="{ name: 'home'}">Home</RouterLink> | 
       <RouterLink :to="{ name: 'products'}">전체 상품 목록</RouterLink> | 
-      <!-- <RouterLink :to="{ name: 'profile', params: { userId: id }}">전체 상품 목록</RouterLink> -->
+      <!-- <RouterLink :to="{ name: 'profile', params: { userId: id }}">마이 페이지</RouterLink> -->
       <button @click="logout">로그 아웃</button>
     </div>
   </div>
@@ -25,6 +24,11 @@ import { RouterLink, RouterView } from 'vue-router'
 import { useBankStore } from './stores/bank'
 
 const store = useBankStore()
+
+const logout = function () {
+  console.log('로그 아웃')
+  store.logout()
+}
 </script>
 
 
--- final_pjt/final_pjt_front/src/views/DetailView.vue
+++ final_pjt/final_pjt_front/src/views/DetailView.vue
@@ -1,15 +1,41 @@
 <template>
   <div>
-
+    <h3>금융 상품명</h3>
+    <p>
+      {{ product.fin_prdt_nm }}
+      <button @click="store.interest(product.id)" class="btn">
+        <span v-if="store.interestProdcutsList.includes(product.id)">❤️</span>
+        <span v-else>🤍</span>
+      </button>
+    </p><hr>
+    <h3>금융 회사명</h3>
+    <p>{{ product.kor_co_nm }}</p><hr>
+    <h3>유의 사항</h3>
+    <p>{{ product.etc_note }}</p><hr>
+    <h3>가입 제한</h3>
+    <p>{{ product.join_deny }}</p><hr>
+    <h3>가입 대상</h3>
+    <p>{{ product.join_member }}</p><hr>
+    <h3>가입 방법</h3>
+    <p>{{ product.join_way }}</p><hr>
+    <h3>우대 조건</h3>
+    <p>{{ product.spcl_cnd }}</p><hr>
+    <h3>만기후 이자율</h3>
+    <p>{{ product.mtrt_int }}</p><hr>
   </div>
+  <!-- 같은 회사, 같은 상품 코드 같은 만기후 이자율 등 다른 상품 보기 -->
 </template>
 
 
 <script setup>
 import { useRoute } from 'vue-router'
+import { useBankStore } from '@/stores/bank'
 
 const route = useRoute()
+const store = useBankStore()
 
+const productId = parseInt(route.params.id)
+const product = store.products.find((element) => element.id === productId)
 </script>
 
 
